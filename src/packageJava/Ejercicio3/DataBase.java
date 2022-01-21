8147dfc51514d816ddbcc5ff12dea7ba30e2c754
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-21 19:04:22 +0100

    Exercise3, ud7, finally get all table

--- src/packageJava/Ejercicio3/DataBase.java
+++ src/packageJava/Ejercicio3/DataBase.java
@@ -7,24 +7,14 @@ public class DataBase {
 	
 	public static void start() {
 	Hashtable <String, Integer> products = new Hashtable <String, Integer> ();
-		int cont = 0;
-		//String question = JOptionPane.showInputDialog("Desea ingresar productos?");
-		for(int i = 0; i <= 3; i++) {
-		//if(question.equals("si")) {
-			//do {
+		for(int i = 0; i <= 4; i++) {
 				String product = JOptionPane.showInputDialog("Indique el producto");
 				String quantity = JOptionPane.showInputDialog("Ingrese cantidad");
 				int cantidad = Integer.parseInt(quantity);
 				products.put(product, cantidad);
-				//products.put("chocolate", 8);
-				//products.put("helado", 8);
-				//cont++;
-			//}while(cont < 3);
 		}choice(products);
-		//}else {
 			JOptionPane.showMessageDialog(null, "Gracias, vuelva pronto");
 		}	
-	//}
 	
 	public static void choice(Hashtable <String, Integer> products) {
 		String choice = JOptionPane.showInputDialog("Indique opcion: \n1. Buscar producto  \n2. Ver lista de productos");
@@ -48,6 +38,7 @@ public class DataBase {
 	public static void printData(Hashtable <String, Integer> products) {
 		Enumeration <Integer> elemento = products.elements();
 		Enumeration <String> keys = products.keys();
+		System.out.println("La lista de productos son = \n");
 		while(elemento.hasMoreElements() && keys.hasMoreElements()) {
 			System.out.println("Producto = " + keys.nextElement() + "\nCantidad = " + elemento.nextElement());
 		}
